ee7f6bd56970d8e541059d434a0fa4e86f052d99
Chaeyeon Seo <ssallyseo@gmail.com>
2024-03-25 16:46:53 +0900

    [Refactor] 카테고리 api 폴더 분리

--- src/main/java/PU/pushop/product/controller/ProductApiControllerV1.java
+++ src/main/java/PU/pushop/product/controller/ProductApiControllerV1.java
@@ -133,58 +133,6 @@ public class ProductApiControllerV1 {
         return ResponseEntity.ok().build();
     }
 
-
-    /**
-     * 카테고리
-     */
-
-    // 전체 카테고리 조회 - 자식 카테고리가 한번 더 나오는 문제 때문에 findall 사용하면 안 될 듯
-    /*@GetMapping("/categorys")
-    public ResponseEntity<?> getCategoryList() {
-        return ResponseEntity.ok(productServiceV1.getCategoryList());
-    }*/
-
-    @GetMapping("/categorys")
-    public ResponseEntity<?> getCategoryList() {
-        List<ProductCategory> topLevelCategories = productServiceV1.getTopLevelCategories(); // 최상위 부모 카테고리만 가져오는 메서드
-
-        // 부모 카테고리 리스트를 DTO로 변환
-        List<ProductCategoryDto> categoryDtoList = topLevelCategories.stream()
-                .map(ProductCategoryDto::of)
-                .collect(Collectors.toList());
-
-        return ResponseEntity.ok(categoryDtoList);
-    }
-
-    // 부모 카테고리 생성
-    @PostMapping("/categorys/parent")
-    public ResponseEntity<Long> createParentCategory(@RequestBody ProductCategory category) {
-        Long categoryId = productServiceV1.createCategory(category, null); // 부모 카테고리 생성 시 parentId를 null로 전달
-        return ResponseEntity.ok(categoryId);
-    }
-
-    // 1. 부모 카테고리 id를 URL로 보냄
-    @PostMapping("/categorys/child/{parentId}")
-    public ResponseEntity<Long> createChildCategory(@RequestBody ProductCategory category, @PathVariable Long parentId) {
-        Long categoryId = productServiceV1.createCategory(category, parentId); // 부모 카테고리의 ID를 parentId로 전달하여 자식 카테고리 생성
-        return ResponseEntity.ok(categoryId);
-    }
-
-    // 2. 부모 카테고리 id를 바디로 보냄
-    @PostMapping("/categorys/child")
-    public ResponseEntity<Long> createChildCategory(@RequestBody ProductCategory category) {
-        Long parentId = category.getParent().getCategoryId(); // 요청 바디에 있는 부모 카테고리의 ID를 가져옵니다.
-        Long categoryId = productServiceV1.createCategory(category, parentId); // 부모 카테고리의 ID를 parentId로 전달하여 자식 카테고리 생성
-        return ResponseEntity.ok(categoryId);
-    }
-
-    // 3. 부모 카테고리 이름을 바디로 보냄
-
-
-
-
-
-
     /**
      * 색상 등록
      */
--- src/main/java/PU/pushop/product/service/ProductServiceV1.java
+++ src/main/java/PU/pushop/product/service/ProductServiceV1.java
@@ -25,7 +25,6 @@ import java.util.stream.Collectors;
 public class ProductServiceV1 {
     public final ProductRepositoryV1 productRepositoryV1;
 
-    public final ProductCategoryRepository categoryRepository;
     public final ProductColorRepository productColorRepository;
     private final ProductSizeRepository productSizeRepository;
 
@@ -94,45 +93,6 @@ public class ProductServiceV1 {
 
         productRepositoryV1.delete(existingProduct);
     }
-
-    // 전체 카테고리
-    public List<ProductCategory> getTopLevelCategories() {
-        return categoryRepository.findByParentIsNull(); // 부모 카테고리가 없는 경우를 조회하여 최상위 부모 카테고리를 반환
-    }
-
-    @Transactional(rollbackFor = Exception.class)
-    public List<ProductCategoryDto> getCategoryList() {
-        List<ProductCategoryDto> results = categoryRepository.findAll().stream().map(ProductCategoryDto::of).collect(Collectors.toList());
-        return results;
-    }
-
-    @Transactional
-    public Long createCategory(ProductCategory category, Long parentId){
-        if (parentId != null) {
-            // 부모 카테고리가 지정된 경우
-            Optional<ProductCategory> parentOptional = categoryRepository.findById(parentId);
-            /*if (parentOptional.isEmpty()) {
-                // 부모 카테고리가 존재하지 않는 경우 예외 처리
-                throw new ChangeSetPersister.NotFoundException();
-            }*/
-
-            ProductCategory parentCategory = parentOptional.get();
-            category.setParent(parentCategory);
-            category.setDepth(parentCategory.getDepth() + 1); // 자식 카테고리의 depth를 설정합니다.
-            parentCategory.getChildren().add(category);
-        } else {
-            // 부모 카테고리가 지정되지 않은 경우 최상위 카테고리로 설정
-            category.setDepth(0L);
-        }
-
-        ProductCategory savedCategory = categoryRepository.save(category);
-        return savedCategory.getCategoryId();
-    }
-    /*@Transactional
-    public Long createCategory(ProductCategory category) {
-        categoryRepository.save(category);
-        return category.getCategoryId();
-    }*/
 
     @Transactional
     public Long createColor(ProductColor color) {
