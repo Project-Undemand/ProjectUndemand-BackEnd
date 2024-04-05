2d9b47e3572bf0cdea48f848b917281702ecb4a2
Chaeyeon Seo <ssallyseo@gmail.com>
2024-04-05 22:22:43 +0900

    [Feat] 주문 로직 구현

---
+++ src/main/java/PU/pushop/order/controller/OrderController.java
@@ -0,0 +1,87 @@
+package PU.pushop.order.controller;
+
+import PU.pushop.order.entity.Orders;
+import PU.pushop.order.entity.enums.PayMethod;
+import PU.pushop.order.service.OrderService;
+import jakarta.servlet.http.HttpSession;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+//@RestController
+@RestController
+@RequestMapping("/api/v1/order")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+    private HttpSession httpSession;
+
+    /**
+     * 주문서에 나타낼 정보
+     * @param payload
+     * @return
+     */
+    @PostMapping("/create")
+    public ResponseEntity<Orders> createOrder(@RequestBody Map<String, Object> payload) {
+        List<Long> cartIds = (List<Long>) payload.get("cartIds");
+        Orders temporaryOrder = orderService.createOrder(cartIds);
+
+        // 세션에 임시 주문 정보를 저장
+        httpSession.setAttribute("temporaryOrder", temporaryOrder);
+
+        return ResponseEntity.ok(temporaryOrder);
+    }
+
+    @Data
+    static class OrderRequest{
+        private String postCode;
+        private String address;
+        private String detailAddress;
+        private String ordererName;
+        private String phoneNumber;
+        PayMethod payMethod;
+    }
+    private Orders RequestForm(OrderRequest request){
+        Orders orders = new Orders();
+
+        orders.setPostCode(request.postCode);
+        orders.setAddress(request.address);
+        orders.setDetailAddress(request.detailAddress);
+        orders.setOrdererName(request.ordererName);
+        orders.setPhoneNumber(request.phoneNumber);
+        orders.setPayMethod(request.payMethod);
+
+        return orders;
+    }
+
+    /**
+     * 주문서에서 입력받아 최종 주문 테이블 생성
+     * @param request
+     * @return
+     */
+    @PostMapping("/done")
+    public ResponseEntity<Orders> completeOrder(@RequestBody OrderRequest request) {
+
+        Orders orders = RequestForm(request);
+
+        // 세션에서 임시 주문 정보를 가져옴
+        Orders temporaryOrder = (Orders) httpSession.getAttribute("temporaryOrder");
+        if (temporaryOrder == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        Orders completedOrder = orderService.orderConfirm(temporaryOrder, orders);
+
+        // 세션에서 임시 주문 정보 삭제
+        httpSession.removeAttribute("temporaryOrder");
+
+        // 장바구니에서 삭제하는 로직 추가해야 함
+
+        return ResponseEntity.ok(completedOrder);
+    }
+}
