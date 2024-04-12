0c0731fbf6e7490d05cf48ee008ecd6b72b64341
Chaeyeon Seo <ssallyseo@gmail.com>
2024-04-12 13:38:44 +0900

    [Modify] 이미 담은 상품,옵션을 장바구니에 담는 경우 수량, 가격을 수정해서 저장

--- src/main/java/PU/pushop/cart/repository/CartRepository.java
+++ src/main/java/PU/pushop/cart/repository/CartRepository.java
@@ -2,10 +2,12 @@ package PU.pushop.cart.repository;
 
 import PU.pushop.cart.entity.Cart;
 import PU.pushop.product.entity.Product;
+import PU.pushop.productManagement.entity.ProductManagement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart,Long> {
@@ -14,5 +16,7 @@ public interface CartRepository extends JpaRepository<Cart,Long> {
     List<Cart> findByCartIdIn(List<Long> cartIds);
     Cart findByMemberIdAndCartId(Long memberId, Long cartId);
     List<Product> findAllProductByCartIdIn(List<Long> cartIds);
+
+    Optional<Cart> findByProductManagement(ProductManagement productManagement);
 
 }
