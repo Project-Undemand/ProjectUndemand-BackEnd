6a8c6a0fc9bba92faf6ea6a3c34e39bb48974c49
Chaeyeon Seo <ssallyseo@gmail.com>
2024-05-06 18:47:51 +0900

    [Modify] 장바구니 등록, 수량 수정시 수량 1 이상 입력하도록 유효성 검사 추가

--- src/main/java/PU/pushop/cart/model/CartRequestDto.java
+++ src/main/java/PU/pushop/cart/model/CartRequestDto.java
@@ -1,6 +1,8 @@
 package PU.pushop.cart.model;
 
 import PU.pushop.cart.entity.Cart;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +12,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CartRequestDto {
     private Long memberId;
+    @NotNull(message = "수량은 필수로 입력해야 합니다.")
+    @Min(value = 1L, message = "수량은 1 이상이어야 합니다.")
     private Long quantity;
 
     public CartRequestDto(Cart cart) {
