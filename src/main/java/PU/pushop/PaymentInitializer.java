5b9a9addb9c8628934e7d0a7d450f2ba12a3a109
ksw4060 <gokorea1214@kakao.com>
2024-08-08 10:55:31 +0900

    [Feat] PaymentHistory 더미데이터 생성하는 유틸 클래스와 이니셜라이저

--- src/main/java/PU/pushop/PaymentInitializer.java
+++ src/main/java/PU/pushop/PaymentInitializer.java
@@ -1,2 +1,26 @@
-package PU.pushop;public class PaymentInitializer {
+package PU.pushop;
+
+import PU.pushop.global.dummydata.util.OrderDataUtil;
+import PU.pushop.global.dummydata.util.PaymentHistoryDataUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("develop")
+@Order(3) // 다른 순서 지정
+@RequiredArgsConstructor
+public class PaymentInitializer implements ApplicationRunner {
+
+    private final OrderDataUtil orderDataUtil;
+    private final PaymentHistoryDataUtil paymentHistoryDataUtil;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        orderDataUtil.createOrders();
+        paymentHistoryDataUtil.createPaymentHistories();
+    }
 }
--- src/main/java/PU/pushop/global/dummydata/util/PaymentHistoryDataUtil.java
+++ src/main/java/PU/pushop/global/dummydata/util/PaymentHistoryDataUtil.java
@@ -1,9 +1,76 @@
 package PU.pushop.global.dummydata.util;
 
+import PU.pushop.InventoryProduct.entity.InventoryProduct;
+import PU.pushop.InventoryProduct.repository.InventoryProductRepository;
+import PU.pushop.global.ResponseMessageConstants;
+import PU.pushop.members.entity.Member;
+import PU.pushop.members.repository.MemberRepositoryV1;
+import PU.pushop.order.entity.Orders;
+import PU.pushop.order.entity.enums.PayMethod;
+import PU.pushop.order.repository.OrderRepository;
+import PU.pushop.payment.entity.PaymentHistory;
+import PU.pushop.payment.entity.Status;
+import PU.pushop.payment.repository.PaymentRepository;
+import PU.pushop.product.entity.Product;
+import PU.pushop.productManagement.entity.ProductManagement;
+import PU.pushop.productManagement.repository.ProductManagementRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
 @Component
 @RequiredArgsConstructor
-public class PaymentHistoryUtil {
+public class PaymentHistoryDataUtil {
+
+    private final PaymentRepository paymentHistoryRepository;
+    private final OrderRepository orderRepository;
+    private final MemberRepositoryV1 memberRepositoryV1;
+    private final ProductManagementRepository productManagementRepository;
+    private final InventoryProductRepository inventoryProductRepository;
+    private final Random random = new Random();
+
+    @Transactional
+    public void createPaymentHistories() {
+        createPaymentHistoriesForMember("user1@pushop.com");
+        createPaymentHistoriesForMember("user2@pushop.com");
+        createPaymentHistoriesForMember("user3@pushop.com");
+    }
+
+    @Transactional
+    public void createPaymentHistoriesForMember(String email) {
+        Member member = memberRepositoryV1.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(ResponseMessageConstants.MEMBER_NOT_FOUND));
+
+        List<Orders> ordersList = orderRepository.findByMember(member);
+
+        for (Orders order : ordersList) {
+            for (ProductManagement pm : order.getProductManagements()) {
+                Product product = pm.getProduct();
+                String option = pm.getColor().getColor() + ", " + pm.getSize().toString();
+                String address = "pushop 테스트 주소";
+                long quantity = 1 + random.nextInt(5);
+
+                InventoryProduct inventoryProduct = pm.getInventoryProduct();
+
+                PaymentHistory paymentHistory = new PaymentHistory(
+                        "impUid" + pm.getInventoryId(), member, order, product,
+                        product.getProductName(), option, quantity,
+                        pm.getProduct().getPriceToBigDecimal().intValue(),
+                        pm.getProduct().getPriceToBigDecimal().intValue() * (int) quantity,
+                        Status.COMPLETE_PAYMENT, PayMethod.card.toString(),"noBankCode", "noBankName",
+                        address, member.getEmail());
+
+                paymentHistoryRepository.save(paymentHistory);
+
+                // 재고 감소 로직
+                inventoryProduct.setProductStock(inventoryProduct.getProductStock() - quantity);
+                inventoryProductRepository.save(inventoryProduct);
+            }
+        }
+    }
 }
