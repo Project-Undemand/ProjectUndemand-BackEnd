8891beb5b2db41cc39fe81a17ac4f326420fd0a3
ksw4060 <gokorea1214@kakao.com>
2024-08-08 11:09:14 +0900

    [Feat] Inquery 더미데이터 유틸, 이니셜라이저 추가

--- src/main/java/PU/pushop/InqueryInitializer.java
+++ src/main/java/PU/pushop/InqueryInitializer.java
@@ -1,2 +1,22 @@
-package PU.pushop;public class InqueryInitializer {
+package PU.pushop;
+
+import PU.pushop.global.dummydata.util.InquiryDataUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("develop")
+@Order(4) // 다른 순서 지정
+@RequiredArgsConstructor
+public class InqueryInitializer implements ApplicationRunner {
+
+    private final InquiryDataUtil inquiryDataUtil;
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        inquiryDataUtil.createInquiries();
+    }
 }
--- src/main/java/PU/pushop/global/dummydata/util/InquiryDataUtil.java
+++ src/main/java/PU/pushop/global/dummydata/util/InquiryDataUtil.java
@@ -1,2 +1,91 @@
-package PU.pushop.global.dummydata.util;public class InquiryDataUtil {
+package PU.pushop.global.dummydata.util;
+
+
+import PU.pushop.Inquiry.entity.Inquiry;
+import PU.pushop.Inquiry.entity.enums.InquiryType;
+import PU.pushop.Inquiry.repository.InquiryRepository;
+import PU.pushop.members.entity.Member;
+import PU.pushop.members.repository.MemberRepositoryV1;
+import PU.pushop.product.entity.Product;
+import PU.pushop.product.repository.ProductRepositoryV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class InquiryDataUtil {
+
+    private final InquiryRepository inquiryRepository;
+    private final MemberRepositoryV1 memberRepositoryV1;
+    private final ProductRepositoryV1 productRepository;
+
+    public void createInquiries() {
+        List<String> emails = Arrays.asList("user1@pushop.com", "user2@pushop.com", "user3@pushop.com");
+
+        for (String email : emails) {
+            Member member = memberRepositoryV1.findByEmail(email)
+                    .orElseThrow(() -> new UsernameNotFoundException("Member not found"));
+
+            List<Product> products = productRepository.findAll();
+
+            for (Product product : products) {
+                for (int i = 0; i < 2; i++) {
+                    InquiryType inquiryType = InquiryType.values()[(int) (Math.random() * 5)]; // RANDOMLY SELECT INQUIRY TYPE BETWEEN 0 AND 4
+                    String title = getInquiryTitle(inquiryType, product.getProductId(), i);
+                    String content = getInquiryContent(inquiryType, product.getProductId(), i);
+
+                    Inquiry inquiry = Inquiry.builder()
+                            .member(member)
+                            .product(product)
+                            .name(member.getNickname())
+                            .email(member.getEmail())
+                            .inquiryType(inquiryType)
+                            .inquiryTitle(title)
+                            .inquiryContent(content)
+                            .password("password")
+                            .build();
+
+                    inquiryRepository.save(inquiry);
+                }
+            }
+        }
+    }
+
+    private String getInquiryTitle(InquiryType inquiryType, Long productId, int index) {
+        switch (inquiryType) {
+            case PRODUCT:
+                return "Product 에 대한 문의 " + productId + " - " + index;
+            case DELIVERY:
+                return "Delivery 에 대한 문의 " + productId + " - " + index;
+            case RETURN:
+                return "Return 에 대한 문의 " + productId + " - " + index;
+            case EXCHANGE:
+                return "Exchange 에 대한 문의 " + productId + " - " + index;
+            case REFUND:
+                return "Refund 에 대한 문의 " + productId + " - " + index;
+            default:
+                return "Inquiry Title " + productId + " - " + index;
+        }
+    }
+
+    private String getInquiryContent(InquiryType inquiryType, Long productId, int index) {
+        switch (inquiryType) {
+            case PRODUCT:
+                return "Product 에 대한 문의 입니다. 어떻게 하면 될까요?? 답변 부탁드립니다. " + productId + " - " + index;
+            case DELIVERY:
+                return "Delivery 에 대한 문의 입니다. 어떻게 하면 될까요?? 답변 부탁드립니다. " + productId + " - " + index;
+            case RETURN:
+                return "Return 에 대한 문의 입니다. 어떻게 하면 될까요?? 답변 부탁드립니다. " + productId + " - " + index;
+            case EXCHANGE:
+                return "Exchange 에 대한 문의 입니다. 어떻게 하면 될까요?? 답변 부탁드립니다. " + productId + " - " + index;
+            case REFUND:
+                return "Refund 에 대한 문의 입니다. 어떻게 하면 될까요?? 답변 부탁드립니다. " + productId + " - " + index;
+            default:
+                return "Inquiry Content " + productId + " - " + index;
+        }
+    }
 }
